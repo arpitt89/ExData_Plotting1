14c98eb5efc5e40e7d8a1db9e11adb6a5dedd8aa
arpitt89 <arpitthakur1988@gmail.com>
2015-05-11 00:50:06 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -7,7 +7,7 @@ data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec="
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 ## PART B 
-## PLOTS THE HISTOGRAM AND SAVES IT IN THE DERIRED FORMAT
+## PLOTS THE GRAPH AND SAVES IT IN THE DERIRED FORMAT
 #str(subSetData)
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(subSetData$Global_active_power)
